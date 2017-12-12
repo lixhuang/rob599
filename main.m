224bd7919bc0ad94bfc2774de7eae1c5b543d440
ys <lixhuang@umich.edu>
2017-12-12 01:54:48 -0500

    overall testing

--- main.m
+++ main.m
@@ -6,13 +6,15 @@
 br = TestTrack.br;
 bl = TestTrack.bl;
 bc = TestTrack.cline;
+b_theta = TestTrack.theta;
 
 T_STEP = 0.1;
+SIM_STEP = 0.01;
 C_STEP = 0.2;
-T_LENGTH = 300;
-HORIZON_STEP = 25;
-U1_SCALE = 1;
-U2_SCALE = 0.001;
+T_LENGTH = 70;
+HORIZON_STEP = 50;
+U1_SCALE = 30;
+U2_SCALE = 0.00005;
 
 
 param.T_STEP = T_STEP;
@@ -29,9 +31,10 @@ end
 param.len = HORIZON_STEP;
 
 % x, u, y, v, phi, r
-%x_init = [287; 5; -176; 0; 2; 0];
-x_init = [255; 10; -91; 0; 2; 0];
-%x_init = [650.078934737117;9.96237194981144;461.327390311492;0.188743033523074;0.465875093881878;0.101631509480780];
+x_init = [287; 5; -176; 0; 2; 0];
+%x_init = [253.5; 10; -94; 0; 1.9; 0];
+%x_init = [562.55;14.51;316.84;-2.09;1.01;0.24];
+%x_init = [878.343380813432;24.6901208124630;458.223577742850;-0.885645635966873;0.836579999546011;0.114723547684054];
 lb = ones([HORIZON_STEP*2,1]); 
 lb(1:2:end) = lb(1:2:end)*-0.5*U1_SCALE;
 lb(2:2:end) = lb(2:2:end)*-10000*U2_SCALE;
@@ -40,33 +43,57 @@ ub(1:2:end) = ub(1:2:end)*0.5*U1_SCALE;
 ub(2:2:end) = ub(2:2:end)*5000*U2_SCALE;
 
 x = x_init;
-%id = 1;
-id = 5;
-%id = 99;
+id = 1;
+%id = 7;
+%id = 69;
+%id = 140;
 u = ones([HORIZON_STEP*2,1]);
 u(1:2:end) = u(1:2:end)*0*U1_SCALE;
 u(2:2:end) = u(2:2:end)*100*U2_SCALE;
 x_log = [];
+u_log = [];
 
-for t = 0:T_STEP:T_LENGTH
+for t = 0:SIM_STEP:T_LENGTH
     
     x_log = [x_log,x];
     
     %% prepare initial guess
     %x0 = x;
+    prox_len =60;
     param.x0 = x;
+    param.prox_len =prox_len;
+    param.bc = bc(:,id:id+prox_len-1);
+    param.theta = b_theta(:,id:id+prox_len-1);
     u0 = [u(3:end);u(end-1:end)];
     
-    prox_len = 5;
-    px = bc(1,id:id+prox_len-1);
-    py = bc(2,id:id+prox_len-1);
-    A=[px;px.^2;py;py.^2]';
-    b = -1*ones([prox_len,1]);
-    a = (A'*A)^-1*A'*b;
-    param.a = a;
+    %px = bc(1,id:id+prox_len-1)-bc(1,id-1);
+    %py = bc(2,id:id+prox_len-1)-bc(2,id-1);
+    %plx = bl(1,id:id+prox_len-1)-bc(1,id-1);
+    %ply = bl(2,id:id+prox_len-1)-bc(2,id-1);
+    %prx = br(1,id:id+prox_len-1)-bc(1,id-1);
+    %pry = br(2,id:id+prox_len-1)-bc(2,id-1);
+    %A=[px;px.^2;py;py.^2;px.*py;px.^3;py.^3;px.*py.^2;py.*px.^2]';%;px.^4;px.^3.*py;px.^2.*py.^2;px.*py.^3;py.^4]';
+    %Al=[plx;plx.^2;ply;ply.^2;plx.*ply;plx.^3;ply.^3;plx.*ply.^2;ply.*plx.^2]';
+    %Ar=[prx;prx.^2;pry;pry.^2;prx.*pry;prx.^3;pry.^3;prx.*pry.^2;pry.*prx.^2]';
+    %b = 1*ones([prox_len,1]);
+    %a = (A'*A)^-1*A'*b; %overdetermined
+    %al = (Al'*Al)^-1*Al'*b; %overdetermined
+    %ar = (Ar'*Ar)^-1*Ar'*b; %overdetermined
+    
+    %a = A^-1*b; %exact
+    %al = Al^-1*b; %exact
+    %ar = Ar^-1*b; %exact
+    
+    %ar = Ar'*(Ar*Ar')^-1*b;
+    %al = Al'*(Al*Al')^-1*b;
+    %a = A'*(A*A')^-1*b;
+    %param.a = a;
         
-    option = optimoptions('fmincon','MaxFunctionEvaluations',10000);
-    u = fmincon(@(x)cost(x,param), u0, [], [], [], [], lb, ub, @(x)nonlcon(x,param),option);
+    option = optimoptions('fmincon','MaxFunctionEvaluations',25000);
+    if(floor(t*10)==t*10)
+        u = fmincon(@(x)cost(x,param), u0, [], [], [], [], lb, ub, @(x)nonlcon(x,param),option);
+        t
+    end
     
     debug_log=[];
     x_temp = x;
@@ -82,6 +109,8 @@ for t = 0:T_STEP:T_LENGTH
     clf
     plot(bc(1,:),bc(2,:))
     hold on;
+    plot(bl(1,:),bl(2,:),'k')
+    plot(br(1,:),br(2,:),'k')
     %aprox_center = get_proxline([px(1);py(1)],[px(end);py(end)],a);
     %plot(aprox_center(1,:),aprox_center(2,:),'g-')
     plot(debug_log(1,:),debug_log(3,:),'r')
@@ -93,7 +122,8 @@ for t = 0:T_STEP:T_LENGTH
     
     
     %% evolve system and found nearset point
-    x = f_car(x,[u(1)/U1_SCALE;u(2)/U2_SCALE], T_STEP);
+    u_log = [u_log,[u(1)/U1_SCALE;u(2)/U2_SCALE]];
+    x = f_car(x,[u(1)/U1_SCALE;u(2)/U2_SCALE], SIM_STEP);
     d = inf;
     for i = id:id+10
         if(i>length(bc))
@@ -105,9 +135,9 @@ for t = 0:T_STEP:T_LENGTH
             id = i;
         end
     end
-    t
+    
     [c,ceq] = nonlcon(u,param);
-    d_max_debug = max(c)
+    %d_max_debug = max(c)
 end
 
 
--- nonlcon.m
+++ nonlcon.m
@@ -3,7 +3,7 @@ function [c,ceq] = nonlcon(u,param)
     HORIZON_STEP = param.HORIZON_STEP;
     T_STEP = param.T_STEP;
     C_STEP = param.C_STEP;
-    a = param.a;
+    %a = param.a;
     U1_SCALE = param.U1_SCALE;
     U2_SCALE = param.U2_SCALE;
     
@@ -11,44 +11,55 @@ function [c,ceq] = nonlcon(u,param)
     u(2:2:end) = u(2:2:end)/param.U2_SCALE;
     
     d_vec = zeros([param.len,1]);
+    theta_vec = zeros([param.len,1]);
     
     x = param.x0;
-    x_log = [];
+    bc = param.bc;
+    b_theta = param.theta;
+    prox_len = param.prox_len;
+    
+    x_log = zeros([6,HORIZON_STEP]);
+    id = 2;
     it = 1;
     %for i = 0:T_STEP:HORIZON_STEP*C_STEP-T_STEP
     for uid = 1:HORIZON_STEP
-        x_log = [x_log,x];
         %uid = floor(i/C_STEP)+1;
         x = f_car(x,u(uid*2-1:uid*2),T_STEP);
+        x_log(:,uid) = x;
         
         %% find distance
-        x0 = x(1);
-        y0 = x(3);
-
-        A1 = -(4*a(2)*y0+2*a(3));
-        A2 = 2*a(3)*x0-2*a(1)*y0;
-        A3 = 4*a(4)-4*a(2);
-        A4 = -4*a(4)*x0-2*a(1);
-
-        G4 = a(2)*A3^2;
-        G3 = a(1)*A3^2 + 2*a(2)*A3*A4;
-        G2 = a(2)*A4^2 + 2*a(1)*A3*A4 + a(3)*A1*A3 + a(4)*A1^2 + A3^2;
-        G1 = a(1)*A4^2 + a(3)*A1*A4 + a(3)*A2*A3 + 2*a(4)*A1*A2 + 2*A3*A4;
-        G0 = a(4)*A2^2 + a(3)*A2*A4 + A4^2;
-
-        opx = x0;
-        for opi = 1:20
-            opx = opx - (G4*opx^4+G3*opx^3+G2*opx^2+G1*opx+G0)/(4*G4*opx^3+3*G3*opx^2+2*G2*opx^1+G1);
-        end
-        opy = (A1*opx+A2)/(A3*opx+A4);
+        q = [x(1);x(3)];
         
-        d = (x0-opx)^2+(y0-opy)^2;
+        temp_id = id;
+        d1 = sum((bc(:,temp_id)-q).^2);
+        for i = 1:3
+            d2 = sum((bc(:,temp_id+i)-q).^2);
+            if(d2<d1)
+                d1 = d2;
+                temp_id = temp_id+1;
+            end
+        end
+        if(id+1<prox_len)
+            d1 = sum((bc(:,temp_id-1)-q).^2);
+            d2 = sum((bc(:,temp_id+1)-q).^2);
+            if(d2 < d1)
+                id = temp_id + 1;
+            else
+                id = temp_id;
+            end
+        end
+        v1 = bc(:,id) - bc(:,id-1);
+        v2 = q - bc(:,id-1);
+        d = v2'*v2-(v1'*v2)^2/(v1'*v1);
+        
         d_vec(uid) = (sqrt(d)-11.5/2)^1;
+        theta_vec(uid) = wrapToPi(x(5) - (b_theta(id) + b_theta(id-1))/2);
         
         it = it+1;
     end
-    
-    c = d_vec;
+    %v = sqrt(x_log(2,:).^2 + x_log(4,:).^2);
+    v = abs(x_log(4,:));
+    c = 100*[d_vec;v'-3;-x_log(2,:)'];
     
     
     
